662cf233615d11f016ec63b8c5b8d5c60725392f
Tedesca <tedesca7@outlook.com>
2022-09-14 21:57:14 +0200

    Update implementation

--- src/aihr.logic.test/Program.cs
+++ src/aihr.logic.test/Program.cs
@@ -1,4 +1,11 @@
 ﻿using aihr.logic.test;
 using System.Runtime.CompilerServices;
 
-List<CelestialBody> body = new List<CelestialBody>();   
+List<CelestialBody> body = new List<CelestialBody>();
+
+foreach (CelestialBody c in body)
+{
+    Console.WriteLine($"{c.Name}{c.ObitalPeriod}...list continues");
+}
+
+body.Min(x=> x.ObitalPeriod);
